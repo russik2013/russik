085ffdae348609eb4d80961023833fcd3b3f2e38
Basilliy <v-chirko@mail.ru>
2015-12-23 23:42:44 +0200

    Version Vasya 0.1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("our first project");
         print pr = new print();
-        Vasja vs = new Vasja();
+        Vasya vs = new Vasya();
 
     }
 }
---
+++ src/com/company/Vasya.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Vasya extends print {
+    public Vasya(){
+        System.out.println("This is class for Vasya");
+    }
+}
