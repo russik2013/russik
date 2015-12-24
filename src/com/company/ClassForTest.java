a65a73e3fa262b171511a11b590bedf4cbe59d18
russik2013 <z.kon2009@yandex.ru>
2015-12-24 14:33:11 +0300

    new class

--- src/com/company/ClassForTest.java
+++ src/com/company/ClassForTest.java
@@ -1,7 +1,11 @@
 package com.company;
 
 public class ClassForTest {
- public int ComBackNumber(){
-     return 0;
- }
+public int ComBackNumber(){
+        return 0;
+    }
+    public void PrintMyName(){
+        System.out.println("Ruslan");
+    }
+
 }
